5bcb1e11b41feae34a4722a2e28e1aaf4a7ce577
jOh <j0h@example.com>
2022-12-01 22:48:51 +0200

    a5r

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 //api
+let productsaerch = ["mahmoud","ram"]
 
 async function fetchData() {
   let cont = document.querySelector(".cont");
@@ -10,8 +11,12 @@ fetch("https://dummyjson.com/products?limit=100")
  .then((data) => data.json())
 
     .then(({products}) => {
-      console.log(products)
+     
       products.forEach((pro) => {
+       
+       
+        productsaerch.push(pro.title)
+        
         let div = document.createElement("div");
         let img = document.createElement("img");
         let p = document.createElement("p");
@@ -28,8 +33,12 @@ fetch("https://dummyjson.com/products?limit=100")
     });
 }
 
+
+
+
 window.addEventListener("load", () => {
   fetchData();
+
 });
 
 
@@ -183,6 +192,57 @@ contLogin.onclick = function(){
   contLogin.style.display = "none"
 login.style.display =  "none"
 }
+
+
+
+
+
+
+let searsh = document.querySelector(".in input")
+let searshbottun=document.querySelector(".in button")
+
+
+searsh.onfocus=function(){
+
+}
+
+
+searsh.onchange=function(){
+ 
+}
+
+searsh.onkeyup=function(){
+if(searsh.value!==""){
+  
+  let fltierdata=productsaerch.filter(function(ele){
+    return ele.startsWith(searsh.value)
+    
+  });
+ 
+  fltierdata.forEach(function(ele){
+let sarced =document.querySelector(".in-serch-dropmenu")
+
+    let drobmenu= document.createElement("div")
+    drobmenu.className="itmes"
+    let dropmenua=document.createElement("a")
+
+
+    dropmenua.textContent=ele
+drobmenu.append(dropmenua)
+    sarced.append(drobmenu)
+  })
+  console.log(fltierdata)
+}
+if(searsh.value===""){
+  let sarced55 =document.querySelectorAll(".itmes")
+  console.log(sarced55)
+  sarced55.forEach(function(ele){
+  ele.remove()
+  })
+  }
+
+}
+
 
 
 
